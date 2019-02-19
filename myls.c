3816fb5415dc6130e859d66732b99e337d9fbd52
Mathias <mathias.amarillolemos@gmail.com>
2019-02-19 12:54:27 +0100

    No Arg ls running, need to be imporoved over errors. Return -1 needs to be implemented.

--- myls.c
+++ myls.c
@@ -3,9 +3,28 @@
 #include <sys/types.h>          /* Cabeceras llamadas al sistema opendir, readdir y closedir  */
 #include <dirent.h>
 #include <string.h>
+#include <limits.h>
 
 
 int main(int argc, char *argv[])
 {
+  DIR *dir;
+  struct dirent *d;
+  int fileDesc;
+  char buf[PATH_MAX];
+
+
+  if(getcwd(buf, sizeof(buf))!=NULL)
+  {
+    printf("Dentro de my ls\n");
+    printf("Current working directory: %s\n", buf);
+    if((dir = opendir(buf))!=NULL)
+    {
+      printf("Se ha podido abrir el directorio\n");
+      while((d = readdir(dir))!=NULL)
+        printf("%s\n", d->d_name);
+    }
+  }
+
 
 }
