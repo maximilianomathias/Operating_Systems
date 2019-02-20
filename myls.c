7533e7eca251eb2877a8b61e6c2f9ccd14de9980
Mathias <mathias.amarillolemos@gmail.com>
2019-02-20 20:07:59 +0100

    Added the usage 1 & 2. need improving the code an bug fixes.

--- myls.c
+++ myls.c
@@ -4,25 +4,56 @@
 #include <dirent.h>
 #include <string.h>
 #include <limits.h>
+#include <string.h>
 
 
 int main(int argc, char *argv[])
 {
   DIR *dir;
   struct dirent *d;
-  //int fileDesc;
+  int fileDesc;
   char buf[PATH_MAX];
+  int i = 0;
+  char *changeDir;
 
+  // control
+  printf("tamaño de path max: %d\n", PATH_MAX);
+  printf("Numero de argumntos: %d\n", argc);
 
-  if(getcwd(buf, sizeof(buf))!=NULL)
-  {
-    printf("Dentro de my ls\n");
-    printf("Current working directory: %s\n", buf);
-    if((dir = opendir(buf))!=NULL)
+  if(argc == 1){ // control number of arguments
+    if(getcwd(buf, sizeof(buf))!=NULL)
     {
-      printf("Se ha podido abrir el directorio\n");
-      while((d = readdir(dir))!=NULL)
-        printf("%s\n", d->d_name);
+      printf("Current working directory: %s\n", buf);
+
+      if((dir = opendir(buf))!=NULL)
+      {
+        while((d = readdir(dir))!=NULL)
+          printf("%s\n", d->d_name);
+      }else
+        return -1;
     }
+    close(dir);
+
   }
+  if(argc == 2){
+    printf("Dentro de los dos argumentos\n");
+    if(getcwd(buf, sizeof(buf))!=NULL)
+      printf("OLD working directory: %s\n", buf);
+    while(argv[i]!=NULL){
+      printf("Argumentos: %s\n", argv[i]);
+      i++;
+    }
+    int a = chdir(argv[1]);
+    if(getcwd(buf, sizeof(buf))!=NULL)
+      printf("NEW working directory: %s\n", buf);
+
+      if((dir = opendir(buf))!=NULL)
+      {
+        while((d = readdir(dir))!=NULL)
+          printf("%s\n", d->d_name);
+      }else
+        return -1;
+  }
+
+  return 0;
 }
