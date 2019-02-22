8be265b8b0aa2fd052bd2a86610a5154820a6557
Mathias <mathias.amarillolemos@gmail.com>
2019-02-22 13:53:23 +0100

    Modifies myls for a cleaner state

--- myls.c
+++ myls.c
@@ -1,59 +1,50 @@
-#include <stdio.h>              /* Cabecera llamada al sistema printf  */
-#include <unistd.h>             /* Cabecera llamada al sistema gtcwd  */
-#include <sys/types.h>          /* Cabeceras llamadas al sistema opendir, readdir y closedir  */
-#include <dirent.h>
-#include <string.h>
-#include <limits.h>
-#include <string.h>
+#include <stdio.h>              /* header library for printf  */
+#include <unistd.h>             /* header library for the system calls gtcwd  */
+#include <sys/types.h>          /* header library for the system calls opendir, readdir y closedir  */
+#include <dirent.h>             /* header library to operate with directories */
+
 
 
 int main(int argc, char *argv[])
 {
   DIR *dir;
   struct dirent *d;
-  int fileDesc;
   char buf[PATH_MAX];
-  int i = 0;
-  char *changeDir;
 
-  // control
-  printf("tamaño de path max: %d\n", PATH_MAX);
-  printf("Numero de argumntos: %d\n", argc);
+  // Controls if the argumets passed are greater that 2
+  if(argc > 2){
+    printf("Number of arguments exceded\n");
+    return -1;
+  }
+  // Changes the current directory if number of argumentes is = 2
+  if(argc == 2){ // control number of arguments
+    printf("Inside the two-argument scope\n");
+    if((chdir(argv[1])!=0)){
+      perror("");
+      return -1;
+    }
+  }
 
-  if(argc == 1){ // control number of arguments
-    if(getcwd(buf, sizeof(buf))!=NULL)
+  if(getcwd(buf, sizeof(buf))!=NULL){
+    printf("Current working directory: %s\n", buf);
+
+    if((dir = opendir(buf))!=NULL)
     {
-      printf("Current working directory: %s\n", buf);
-
-      if((dir = opendir(buf))!=NULL)
-      {
-        while((d = readdir(dir))!=NULL)
-          printf("%s\n", d->d_name);
-      }else
-        return -1;
-    }
-    close(dir);
-
-  }
-  if(argc == 2){
-    printf("Dentro de los dos argumentos\n");
-    if(getcwd(buf, sizeof(buf))!=NULL)
-      printf("OLD working directory: %s\n", buf);
-    while(argv[i]!=NULL){
-      printf("Argumentos: %s\n", argv[i]);
-      i++;
-    }
-    int a = chdir(argv[1]);
-    if(getcwd(buf, sizeof(buf))!=NULL)
-      printf("NEW working directory: %s\n", buf);
-
-      if((dir = opendir(buf))!=NULL)
-      {
-        while((d = readdir(dir))!=NULL)
-          printf("%s\n", d->d_name);
-      }else
-        return -1;
-  }
+      while((d = readdir(dir))!=NULL)
+        printf("%s\n", d->d_name);
+    }else
+      return -1;
+  }else
+    return -1;
 
   return 0;
 }
+
+// notes
+
+/*
+int i = 0;
+while(argv[i]!=NULL){
+printf("Argumentos: %s\n", argv[i]);
+i++;
+}*/
